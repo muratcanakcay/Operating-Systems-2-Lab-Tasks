9d9fe526f2e91690b88534bd0bde3af520ae6310
muratcanakcay <muratcanakcay@gmail.com>
2021-03-26 02:17:51 +0100

    Lab 02 - Code cleanup - Starting Stage 4

--- Lab_02_ Message Queues/prog1_stage4.c
+++ Lab_02_ Message Queues/prog1_stage4.c
@@ -9,32 +9,32 @@
 #include <time.h>
 #include <mqueue.h>
 
+#define HERE puts("**************HERE***************");
+#define DEBUG 0
 #define MAXLENGTH 100
+#define MAXCAPACITY 10
+#define MSG_CHECK_STATUS "check status"
+#define MSG_REGISTER "register"
 
-#define LIFE_SPAN 10
-#define MAX_NUM 10
 
 #define ERR(source) (fprintf(stderr,"%s:%d\n",__FILE__,__LINE__),\
                      perror(source),kill(0,SIGKILL),\
 		     		     exit(EXIT_FAILURE))
 
-
-
-void usage(void){
+void usage(void) // TODO: update for stage 4
+{
 	fprintf(stderr,"USAGE: prog1_stage2 q0_name \n");	
 	exit(EXIT_FAILURE);
 }
 
-void sigchld_handler(int sig) {
+void sigchld_handler(int sig) 
+{
 	pid_t pid;	
 
 	for(;;)
     {
 		pid=waitpid(0, NULL, WNOHANG);
-		if(pid>0){
-            
-            puts("child terminated");
-        }
+		if(pid > 0 && DEBUG) puts("[DEBUG] Prog1 child terminated");
         if(pid == 0) return;
 		if(pid <= 0) 
         {
@@ -42,75 +42,73 @@ void sigchld_handler(int sig) {
 			ERR("waitpid");
 		}
 	}
-	
 }
 
-void sethandler( void (*f)(int), int sigNo) {
+void sethandler( void (*f)(int), int sigNo) 
+{
 	struct sigaction act;
 	memset(&act, 0, sizeof(struct sigaction));
 	act.sa_handler = f;
 	if (-1==sigaction(sigNo, &act, NULL)) ERR("sigaction");
 }
 
-void child_work(char* register_pid);
+void child_work(char* register_pid)
+{
+    char q_name[MAXLENGTH];
+
+	mqd_t q;
+	struct mq_attr attr; 
+	attr.mq_maxmsg = MAXCAPACITY; 
+	attr.mq_msgsize = MAXLENGTH; 
+	
+	// open /q<PID>
+	snprintf(q_name, MAXLENGTH, "/q%s", register_pid);
+	if( (q = TEMP_FAILURE_RETRY(mq_open(q_name, O_WRONLY | O_CREAT, 0600, &attr))) == (mqd_t)-1) ERR("prog1 child mq_open q");
+	
+	// send "check status" message to prog2
+	if(TEMP_FAILURE_RETRY(mq_send(q, MSG_CHECK_STATUS, strlen(MSG_CHECK_STATUS), 0))) ERR("prog1 child mq_send");
+	printf("Message sent on %s : \"%s\"\n", q_name, MSG_CHECK_STATUS);
+	
+	// close /q<PID>
+	if (mq_close(q) < 0) ERR("prog1 child mq_close");
+
+	exit(EXIT_SUCCESS);
+}
 
 int main(int argc, char** argv) 
 {
 	if(argc!=2) usage();
 
-	char q0_name[MAXLENGTH];
-    snprintf(q0_name, MAXLENGTH, "/%s", argv[1]);
-	
-	mqd_t q0; // descriptors
-	struct mq_attr attr; // attributes
-	attr.mq_maxmsg = 10; // capacity
-	attr.mq_msgsize = MAXLENGTH; // max msg size in bytes
-	if((q0=TEMP_FAILURE_RETRY(mq_open(q0_name, O_RDWR | O_CREAT, 0600, &attr)))==(mqd_t)-1) ERR("prog2 mq_open q0");
+	char q0_name[MAXLENGTH], q_name[MAXLENGTH], message[MAXLENGTH];;
+    int msgLength;
+	pid_t pid;
 
-	printf("prog1: message queue q0 with name: %s opened\n", q0_name);
+	mqd_t q0; 
+	struct mq_attr attr;
+	attr.mq_maxmsg = MAXCAPACITY; 
+	attr.mq_msgsize = MAXLENGTH; 
 
-    sethandler(sigchld_handler,SIGCHLD);
+	// open q0
+	snprintf(q0_name, MAXLENGTH, "/%s", argv[1]);	
+	if ( (q0 = TEMP_FAILURE_RETRY(mq_open(q0_name, O_RDONLY | O_CREAT, 0600, &attr))) == (mqd_t)-1 ) ERR("prog1 mq_open q0");
 	
-    pid_t pid;
-    
+	// receive messages from prog2 and create child processes
+	sethandler(sigchld_handler,SIGCHLD);
     while(1)
-    {
-        char buffer[MAXLENGTH];
-        mq_receive(q0, buffer, MAXLENGTH ,NULL);
-
-        buffer[sizeof(buffer)-1]='\0';
-        puts(buffer);
-        pid_t pid;
-        if((pid=fork())<0) ERR("fork");
-        if(0==pid) 
-        child_work(buffer+9);
-    }
+	{
+        if ( (msgLength = TEMP_FAILURE_RETRY(mq_receive(q0, message, MAXLENGTH, NULL))) < 1) ERR("prog1 mq_receive");
+		message[msgLength]='\0';
+        printf("Message received from %s : \"%s\"\n", q0_name, message);
+        
+        if ( (pid=fork()) < 0 ) ERR("fork");
+        if (0 == pid) child_work(message+9); // TODO: must be modified to accept all message types
+	}
 	
-	
-	mq_close(q0);
-	//if(mq_unlink(q0_name))ERR("mq unlink");
+	// close q0
+	if (mq_close(q0) < 0) ERR("prog1 mq_close");
+	//if (mq_unlink(q_name) < 0) ERR("mq unlink");
 
-	puts("prog 1 EXITING");
+	while(wait(NULL)>0) if (DEBUG) printf("[DEBUG] Prog 1 child terminated.\n");
 	
 	return EXIT_SUCCESS;
-}
-
-void child_work(char* register_pid)
-{
-    char q_name[MAXLENGTH];
-    snprintf(q_name, MAXLENGTH, "/q%s", register_pid);
-	
-	mqd_t q; // descriptors
-	struct mq_attr attr; // attributes
-	attr.mq_maxmsg = 10; // capacity
-	attr.mq_msgsize = MAXLENGTH; // max msg size in bytes
-	if((q = TEMP_FAILURE_RETRY(mq_open(q_name, O_RDWR | O_CREAT, 0600, &attr)))==(mqd_t)-1) ERR("prog1 child mq_open q");
-
-	printf("prog1 child: message queue with name: %s opened\n", q_name);
-
-    if(TEMP_FAILURE_RETRY(mq_send(q, "check status", MAXLENGTH, 0))) ERR("mq_send");
-	
-	printf("prog1 child: message sent: \"check status\n\"");
-
-    puts("prog 1 child EXITING");
 }
--- Lab_02_ Message Queues/prog2_stage4.c
+++ Lab_02_ Message Queues/prog2_stage4.c
@@ -9,68 +9,56 @@
 #include <time.h>
 #include <mqueue.h>
 
+#define DEBUG 0
 #define MAXLENGTH 100
-
-#define LIFE_SPAN 10
-#define MAX_NUM 10
+#define MAXCAPACITY 100
+#define MSG_CHECK_STATUS "check status"
+#define MSG_REGISTER "register"
 
 #define ERR(source) (fprintf(stderr,"%s:%d\n",__FILE__,__LINE__),\
                      perror(source),kill(0,SIGKILL),\
 		     		     exit(EXIT_FAILURE))
 
-
-
-void usage(void){
+void usage(void) {   // TODO: update for stage 4
 	fprintf(stderr,"USAGE: prog2_stage2 q0_name \n");	
 	exit(EXIT_FAILURE);
 }
-
-
 
 int main(int argc, char** argv) 
 {
 	if(argc!=2) usage();
 
-	char q0_name[MAXLENGTH];
-    snprintf(q0_name, MAXLENGTH, "/%s", argv[1]);
+	char q0_name[MAXLENGTH], q_name[MAXLENGTH], message[MAXLENGTH];
+	int msgLength;
+	pid_t pid = getpid();
 	
-	mqd_t q0; // descriptors
-	struct mq_attr attr; // attributes
-	attr.mq_maxmsg = 10; // capacity
-	attr.mq_msgsize = MAXLENGTH; // max msg size in bytes
-	if((q0=TEMP_FAILURE_RETRY(mq_open(q0_name, O_RDWR, 0600, &attr)))==(mqd_t)-1) ERR("mq open q0");
-
-	printf("prog2 with pid %d: message queue q0 with name: %s opened\n", getpid(), q0_name);
+	mqd_t q0, q; 										
+	struct mq_attr attrq0, attrq; 						
+	attrq0.mq_maxmsg = attrq.mq_maxmsg = MAXCAPACITY;
+	attrq0.mq_msgsize = attrq.mq_msgsize = MAXLENGTH; 	
 	
-
-	char message[MAXLENGTH];
-    snprintf(message, MAXLENGTH, "register %d", getpid());
-
-    if(TEMP_FAILURE_RETRY(mq_send(q0, message, MAXLENGTH, 0))) ERR("mq_send");
+	// open q0 
+	snprintf(q0_name, MAXLENGTH, "/%s", argv[1]);
+	if( (q0 = TEMP_FAILURE_RETRY(mq_open(q0_name, O_WRONLY, 0600, &attrq0))) == (mqd_t)-1 ) ERR("mq open q0");
 	
-	printf("message sent: %s\n", message);
+	// send message to prog1
+	snprintf (message, MAXLENGTH, "%s %d", MSG_REGISTER, pid);
+	if (TEMP_FAILURE_RETRY(mq_send(q0, message, strlen(message), 0))) ERR("mq_send");
+	printf("Message sent on %s : '%s'\n", q0_name, message);
 
-    mqd_t q; // descriptors
-	struct mq_attr attrq; // attributes
-	attr.mq_maxmsg = 10; // capacity
-	attr.mq_msgsize = MAXLENGTH; // max msg size in bytes
-    
-    char q_name[MAXLENGTH];
-    snprintf(q_name, MAXLENGTH, "/q%s", message+9);
-    
-    if((q=TEMP_FAILURE_RETRY(mq_open(q_name, O_RDWR, 0600, &attrq)))==(mqd_t)-1) ERR("mq open q");
-    printf("prog2 with pid %d: message queue with name: %s opened\n", getpid(), q_name);
+    // open /q<PID>
+	snprintf (q_name, MAXLENGTH, "/q%d", pid);
+    if ( (q = TEMP_FAILURE_RETRY(mq_open(q_name, O_RDONLY, 0600, &attrq))) == (mqd_t)-1 ) ERR("mq open q");
+    if (DEBUG) printf("[DEBUG] Prog2 with pid %d: opened message queue with name: '%s'\n", getpid(), q_name);
 
-	char buffer[MAXLENGTH];
-    mq_receive(q, buffer, MAXLENGTH ,NULL);
-	buffer[sizeof(buffer)-1]='\0';
-    printf("Prog2 received message: %s\n", buffer);
+	// receive message from /q<PID>
+	if ( (msgLength = TEMP_FAILURE_RETRY(mq_receive(q, message, MAXLENGTH ,NULL))) < 1) ERR("mq_receive");
+	message[msgLength]='\0';
+    printf("Message received on %s : '%s'\n", q_name, message);
     
-    
-    mq_close(q0);
-    //if(mq_unlink(q0_name))ERR("mq unlink");
-
-    puts("prog 2 EXITING");
+    // close q0 and /q<PID>
+    if (mq_close(q0)) ERR("mq_close");
+	if (mq_close(q)) ERR("mq_close");
 	
 	return EXIT_SUCCESS;
 }
