37b07bc3fbab8f207d2b4b5345e67c436b2b0e52
muratcanakcay <muratcanakcay@gmail.com>
2021-04-15 22:54:02 +0200

    Task 2

---
+++ Lab 03 Tutorial Task 02/prog24c.c
@@ -0,0 +1,120 @@
+#define _GNU_SOURCE 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <netinet/in.h>
+#include <signal.h>
+#include <netdb.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#define ERR(source) (perror(source),\
+		     fprintf(stderr,"%s:%d\n",__FILE__,__LINE__),\
+		     exit(EXIT_FAILURE))
+#define MAXBUF 576
+volatile sig_atomic_t last_signal=0 ;
+void sigalrm_handler(int sig) {
+	last_signal=sig;
+}
+int sethandler( void (*f)(int), int sigNo) {
+	struct sigaction act;
+	memset(&act, 0, sizeof(struct sigaction));
+	act.sa_handler = f;
+	if (-1==sigaction(sigNo, &act, NULL))
+		return -1;
+	return 0;
+}
+int make_socket(void){
+	int sock;
+	sock = socket(PF_INET,SOCK_DGRAM,0);
+	if(sock < 0) ERR("socket");
+	return sock;
+}
+struct sockaddr_in make_address(char *address, char *port){
+	int ret;
+	struct sockaddr_in addr;
+	struct addrinfo *result;
+	struct addrinfo hints = {};
+	hints.ai_family = AF_INET;
+	if((ret=getaddrinfo(address,port, &hints, &result))){
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(ret));
+		exit(EXIT_FAILURE);
+	}
+	addr = *(struct sockaddr_in *)(result->ai_addr);
+	freeaddrinfo(result);
+	return addr;
+}
+ssize_t bulk_read(int fd, char *buf, size_t count){
+	int c;
+	size_t len=0;
+	do{
+		c=TEMP_FAILURE_RETRY(read(fd,buf,count));
+		if(c<0) return c;
+		if(0==c) return len;
+		buf+=c;
+		len+=c;
+		count-=c;
+	}while(count>0);
+	return len ;
+}
+void usage(char * name){
+	fprintf(stderr,"USAGE: %s domain port file \n",name);
+}
+void sendAndConfirm(int fd,struct sockaddr_in addr,char *buf1, char *buf2, ssize_t size){
+	struct itimerval ts;
+	if(TEMP_FAILURE_RETRY(sendto(fd,buf1,size,0,&addr,sizeof(addr)))<0) ERR("sendto:");
+	memset(&ts, 0, sizeof(struct itimerval));
+	ts.it_value.tv_usec=500000;
+	setitimer(ITIMER_REAL,&ts,NULL);
+	last_signal=0;
+	while(recv(fd,buf2,size,0)<0){
+		if(EINTR!=errno)ERR("recv:");
+		if(SIGALRM==last_signal) break;
+	}
+}
+void doClient(int fd, struct sockaddr_in addr, int file){
+	char buf[MAXBUF];
+	char buf2[MAXBUF];
+	int offset = 2*sizeof(int32_t);
+	int32_t chunkNo=0;
+	int32_t last=0;
+	ssize_t size;
+	int counter;
+	do{
+		if((size=bulk_read(file,buf+offset,MAXBUF-offset))<0)ERR("read from file:");
+		*((int32_t*)buf)=htonl(++chunkNo);
+		if(size<MAXBUF-offset) {
+			last=1;
+			memset(buf+offset+size,0,MAXBUF-offset-size);
+		}
+		*(((int32_t*)buf)+1)=htonl(last);
+		memset(buf2,0,MAXBUF);
+		counter=0;
+		do{
+			counter++;
+			sendAndConfirm(fd,addr,buf,buf2,MAXBUF);
+		}while(*((int32_t*)buf2)!=htonl(chunkNo)&&counter<=5);
+		if(*((int32_t*)buf2)!=htonl(chunkNo)&&counter>5) break;
+	}while(size==MAXBUF-offset);
+}
+int main(int argc, char** argv) {
+	int fd,file;
+	struct sockaddr_in addr;
+	if(argc!=4) {
+		usage(argv[0]);
+		return EXIT_FAILURE;
+	}
+	if(sethandler(SIG_IGN,SIGPIPE)) ERR("Seting SIGPIPE:");
+	if(sethandler(sigalrm_handler,SIGALRM)) ERR("Seting SIGALRM:");
+	if((file=TEMP_FAILURE_RETRY(open(argv[3],O_RDONLY)))<0)ERR("open:");
+	fd = make_socket();
+	addr=make_address(argv[1],argv[2]);
+	doClient(fd,addr,file);
+	if(TEMP_FAILURE_RETRY(close(fd))<0)ERR("close");
+	if(TEMP_FAILURE_RETRY(close(file))<0)ERR("close");
+	return EXIT_SUCCESS;
+}
---
+++ Lab 03 Tutorial Task 02/prog24s.c
@@ -0,0 +1,128 @@
+#define _GNU_SOURCE 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/time.h>
+#include <netinet/in.h>
+#include <signal.h>
+#include <netdb.h>
+#include <fcntl.h>
+#define ERR(source) (perror(source),\
+		     fprintf(stderr,"%s:%d\n",__FILE__,__LINE__),\
+		     exit(EXIT_FAILURE))
+#define BACKLOG 3
+#define MAXBUF 576
+#define MAXADDR 5
+struct connections{
+	int free;
+	int32_t chunkNo;
+	struct sockaddr_in addr;
+};
+int sethandler( void (*f)(int), int sigNo) {
+	struct sigaction act;
+	memset(&act, 0, sizeof(struct sigaction));
+	act.sa_handler = f;
+	if (-1==sigaction(sigNo, &act, NULL))
+		return -1;
+	return 0;
+}
+int make_socket(int domain, int type){
+	int sock;
+	sock = socket(domain,type,0);
+	if(sock < 0) ERR("socket");
+	return sock;
+}
+int bind_inet_socket(uint16_t port,int type){
+	struct sockaddr_in addr;
+	int socketfd,t=1;
+	socketfd = make_socket(PF_INET,type);
+	memset(&addr, 0, sizeof(struct sockaddr_in));
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(port);
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	if (setsockopt(socketfd, SOL_SOCKET, SO_REUSEADDR,&t, sizeof(t))) ERR("setsockopt");
+	if(bind(socketfd,(struct sockaddr*) &addr,sizeof(addr)) < 0)  ERR("bind");
+	if(SOCK_STREAM==type)
+		if(listen(socketfd, BACKLOG) < 0) ERR("listen");
+	return socketfd;
+}
+ssize_t bulk_write(int fd, char *buf, size_t count){
+	int c;
+	size_t len=0;
+	do{
+		c=TEMP_FAILURE_RETRY(write(fd,buf,count));
+		if(c<0) return c;
+		buf+=c;
+		len+=c;
+		count-=c;
+	}while(count>0);
+	return len ;
+}
+int findIndex( struct sockaddr_in addr, struct connections con[MAXADDR]){
+	int i,empty=-1,pos=-1;
+	for(i=0;i<MAXADDR;i++){
+		if(con[i].free) empty=i;
+		else if(0==memcmp(&addr,&(con[i].addr),sizeof(struct sockaddr_in))) {
+			pos=i;
+			break;
+		}
+	}
+	if(-1==pos&&empty!=-1) {
+		con[empty].free=0;
+		con[empty].chunkNo=0;
+		con[empty].addr=addr;
+		pos=empty;
+	}
+	return pos;
+}
+void doServer(int fd){
+	struct sockaddr_in addr;
+	struct connections con[MAXADDR];
+	char buf[MAXBUF];
+	socklen_t size=sizeof(addr);;
+	int i;
+	int32_t chunkNo, last;
+	for(i=0;i<MAXADDR;i++)con[i].free=1;
+	for(;;){
+		if(TEMP_FAILURE_RETRY(recvfrom(fd,buf,MAXBUF,0,&addr,&size)<0)) ERR("read:");
+		if((i=findIndex(addr,con))>=0){
+			chunkNo=ntohl(*((int32_t*)buf));
+			last=ntohl(*(((int32_t*)buf)+1));
+			if(chunkNo>con[i].chunkNo+1) continue;
+			else if(chunkNo==con[i].chunkNo+1) {
+				if(last){
+					printf("Last Part %d\n%s\n",chunkNo,buf+2*sizeof(int32_t));
+					con[i].free=1;
+				}else
+					printf("Part %d\n%s\n",chunkNo,buf+2*sizeof(int32_t));
+				con[i].chunkNo++;
+			}
+			if(TEMP_FAILURE_RETRY(sendto(fd,buf,MAXBUF,0,&addr,size))<0){
+				if(EPIPE==errno) con[i].free=1;
+				else ERR("send:");
+			}
+		}
+		
+	}
+}
+void usage(char * name){
+	fprintf(stderr,"USAGE: %s port\n",name);
+}
+int main(int argc, char** argv) {
+	int fd;
+	if(argc!=2) {
+		usage(argv[0]);
+		return EXIT_FAILURE;
+	}
+	if(sethandler(SIG_IGN,SIGPIPE)) ERR("Seting SIGPIPE:");
+	fd=bind_inet_socket(atoi(argv[1]),SOCK_DGRAM);
+	doServer(fd);
+	if(TEMP_FAILURE_RETRY(close(fd))<0)ERR("close");
+	fprintf(stderr,"Server has terminated.\n");
+	return EXIT_SUCCESS;
+}
