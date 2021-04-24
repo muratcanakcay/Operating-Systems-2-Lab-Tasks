29283ae5c3d7d3392d27781a7ef6e35a9fa7cbb7
muratcanakcay <muratcanakcay@gmail.com>
2021-04-24 20:39:02 +0200

    Lab 03 - Stage 02 Completed

--- Lab 03 Network Sockets/udpfwd_stage2.c
+++ Lab 03 Network Sockets/udpfwd_stage2.c
@@ -16,10 +16,8 @@
 		fprintf(stderr,"%s:%d\n",__FILE__,__LINE__),\
 		exit(EXIT_FAILURE))
 
-#define MAXCON 3
 #define BACKLOG 3
 volatile sig_atomic_t do_work=1;
-
 
 void usage(char * name){
 	fprintf(stderr,"USAGE: %s socket port\n",name);
@@ -111,10 +109,11 @@ void doServer(int fdT){
 	int cfd, cons=0;
 	fd_set base_rfds, rfds;
 	sigset_t mask, oldmask;
-    char data[13] = "hello world\n";
-    char full[6] = "full\n";
-    int con[MAXCON];
-    for (int i = 0; i<MAXCON; i++) con[i] = 0;
+    char data[] = "hello world\n";
+    char full[] = "Max no of clients reached. Connection not accepted.\n";
+    char buf[10];
+    int con[BACKLOG];
+    for (int i = 0; i < BACKLOG; i++) con[i] = -1;
 	
 	// set base_rfds once and use in the loop to reset rfds
 	FD_ZERO(&base_rfds);
@@ -131,30 +130,53 @@ void doServer(int fdT){
     {
 		rfds=base_rfds;
 		
-		if(pselect(fdT+1,&rfds,NULL,NULL,NULL,&oldmask)>0)
+		if (pselect(fdT+1, &rfds, NULL, NULL, NULL, &oldmask) > 0)
         {
-			if(cons < 3 && (cfd = add_new_client(fdT)) > 0)
+            // check for client disconnects
+            for (int i = 0; i < BACKLOG; i++) 
             {
-				cons++;
-                if(bulk_write(cfd, data, 13) < 0 && errno!=EPIPE) ERR("write:");
-			}
-            else if (cons == 3)
-            {
-                // I was trying to check if the received message is zero length which would indicate
-                // that client has closed connection. 
-                
-                
-                char c;
-                ssize_t x = recv(fdT, &c, 1, MSG_PEEK);
-                if (x==0) puts("full");
+                // if recv() call returns zero, it means the connection is closed on the other side
+                if (recv(con[i], buf, sizeof(buf), MSG_PEEK | MSG_DONTWAIT) == 0) 
+                {
+                    close(con[i]);
+                    con[i] = -1;
+                    cons--;
+                }
             }
+            
+            // new client connection
+            if ((cfd = add_new_client(fdT)) > 0)
+            {
+				// if less than 3 clients add new client
+                if(cons < 3)
+                {
+                    cons++;
+                    
+                    int added = 0;
+                    for (int i = 0; i < BACKLOG; i++) 
+                    {
+                        if (con[i] == -1)
+                        {
+                            con[i] = cfd;
+                            added = 1;
+                            break;
+                        }
+                    }
+                    if (!added) ERR("connection add error");
+                    
+                    if(bulk_write(cfd, data, sizeof(data)) < 0 && errno!=EPIPE) ERR("write:");
+                }
+                else // don't add new client, send info msg.
+                {
+                    if (bulk_write(cfd, full, sizeof(full)) < 0 && errno!=EPIPE) ERR("write:");
+    				if (TEMP_FAILURE_RETRY(close(cfd)) < 0) ERR("close");
+                }
+			}
             else
             {
                 if(EINTR==errno) continue;
                 ERR("pselect");
             }
-
-            
         }
 	}
 	sigprocmask (SIG_UNBLOCK, &mask, NULL);
@@ -164,27 +186,27 @@ int main(int argc, char** argv)
 {
 	int fdT;
 	int new_flags;
-	if(argc!=2) 
+	
+    if(argc!=2) 
     {
 		usage(argv[0]);
 		return EXIT_FAILURE;
     }
-	
 
 	if(sethandler(SIG_IGN,SIGPIPE)) ERR("Seting SIGPIPE:");
 	if(sethandler(sigint_handler,SIGINT)) ERR("Seting SIGINT:");
 	
-	// make and bind both local and tcp sockets
+	// make and bind tcp socket
 	fdT=bind_tcp_socket(atoi(argv[1]));
 	
-	// set both local and tcp sockets to nonblocking
+	// set tcp socket to nonblocking
 	new_flags = fcntl(fdT, F_GETFL) | O_NONBLOCK;
 	fcntl(fdT, F_SETFL, new_flags);
 	
 	// accept connections and read data
 	doServer(fdT);
 	
-	if(TEMP_FAILURE_RETRY(close(fdT))<0)ERR("close");
+	if (TEMP_FAILURE_RETRY(close(fdT)) < 0) ERR("close");
 	fprintf(stderr,"Server has terminated.\n");
 	return EXIT_SUCCESS;
 }
